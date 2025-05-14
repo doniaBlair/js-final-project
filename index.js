625eec30dcfe78a04fc02c089669059ac6768700
Donia Blair <donia.blair@gmail.com>
2025-05-14 02:49:00 -0500

    html complete, js functionality outlined

---
+++ index.js
@@ -0,0 +1,35 @@
+// spotify client id: 5a6960985fbc4fd1b21b4a0c0be80839
+// spotify client secret: 1eff2b0a945a4984ab69f5431e49ff35
+
+// {"access_token":"BQD1ZqSHqDTNY-05YmO0ve1ZBAuBhEq5GC2DMZzqmIbtXby4f7u5qSCyAB9Mmdfk7Z8z7SzhLer5UNzDM_X-PxIttQ48s1DvWNNIeo-324EOJI19JoBt8SW1pI7qNFvoi9uJSHw9grY","token_type":"Bearer","expires_in":3600}
+
+// spotify api base: https://api.spotify.com
+// search: https://api.spotify.com/v1/search
+// artists: https://api.spotify.com/v1/artists
+// single artist: https://api.spotify.com/v1/artists/{id}
+// artist's albums: https://api.spotify.com/v1/artists/{id}/albums
+// artist's top tracks: https://api.spotify.com/v1/artists/{id}/top-tracks
+// albums: https://api.spotify.com/v1/albums
+// single album: https://api.spotify.com/v1/albums/{id}
+
+// website requirements:
+// one page
+// search bar
+// search results with search term
+// search filters
+// use data from an api
+// no results message
+// hover states
+// press enter or search button
+// show at least 6 results
+// skeleton loading state
+
+// website ui:
+// on page load, show a list of popular (or random) artists - 12 total in a grid
+// include a search input to search by artist name
+// include a filter to filter by genre
+// include a sort dropdown to sort by artist name, ???
+// information for each artist will include: name, image, genres, number of albums, link to artist page
+// artist page will include name, image, genres, list of top tracks (name only), list of albums
+// information for each album will include: name, image, release date, number of tracks
+// search results will include artists only with same information as above
